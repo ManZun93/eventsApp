c79fe14be3d5134d8f37745ec4caaf68cbb1dcd9
ManZun93 <manuellufan19@gmail.com>
2023-02-15 22:47:52 -0500

    test: probando rutas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <>
-      <div className="text-app-blue">Hola mundo</div>
+      <div className="text-app-blue">Home</div>
+
+      <ul>
+        <li>
+          <Link href="/profiles/profile">ve a tu perfil</Link>
+        </li>
+        <li>
+          <Link href="/profiles/votes/votes">ve a votos</Link>
+        </li>
+        <li>
+          <Link href="/createPost">ve a tus posts</Link>
+        </li>
+        <li>
+          <Link href="/signUp">registrate</Link>
+        </li>
+
+        <li>
+          <Link href="/startSesion">haz login</Link>
+        </li>
+      </ul>
     </>
   );
 }
